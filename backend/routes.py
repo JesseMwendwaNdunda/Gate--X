07908cbc465df2f0d275bc1941bae95f3753c6af
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-09-24 11:38:43 +0300

    install faker

--- backend/routes.py
+++ backend/routes.py
@@ -29,7 +29,7 @@ def login():
     data = request.get_json()
     user = User.query.filter_by(username=data["username"]).first()
 
-    if user and user.check_password(data["password"]):  # ✅ helper method
+    if user and user.check_password(data["password"]): 
         access_token = create_access_token(identity={"id": user.id, "role": user.role})
         return jsonify({"token": access_token, "role": user.role}), 200
 
